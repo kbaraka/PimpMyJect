586e40c050f01ab6bddbc4e4fe82198d6a9f63fd
Benchikhi Taha <benchikhi.taha@gmail.com>
2017-11-17 07:39:43 +0100

    Update app.e2e-spec.ts

--- Sprints/Sprint1/Front-end/e2e/app.e2e-spec.ts
+++ Sprints/Sprint1/Front-end/e2e/app.e2e-spec.ts
@@ -20,7 +20,7 @@ describe('CDP App', () => {
     browser.driver.controlFlow().execute = function () {
       var args = arguments;
       origFn.call(browser.driver.controlFlow(), function () {
-        return protractor.promise.delayed(2);
+        return protractor.promise.delayed(5);
       });
       return origFn.apply(browser.driver.controlFlow(), args);
     };
