185d03050fd1210aa84eb9d20722c1e47a8c6799
Rihab <tlilirihab1@gmail.com>
2017-12-11 23:50:16 +0100

    deleting all

--- Sprints/Sprint1/Sprint1code/Application/Routes/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-
-    require('./login')(app);
-    require('./projet')(app);
-    require('./equipe')(app);
-
-    
-}
--- Sprints/Sprint1/Sprint1code/run.sh
+++
@@ -1,1 +0,0 @@
-ng build -- && rm -r Application/Public/*  && mv ./dist/* Application/Public && rm -r dist && cd Application && npm start
